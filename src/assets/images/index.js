a91c8cb5bf59627146395f0bfa548651c1d57c26
vipin-24 <vipinwild1024@gmail.com>
2024-04-06 14:56:19 +0530

    my-commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,220 @@
+import logo from "./paddedlogo.png";
+import logoLight from "./paddedlogo.png";
+import bannerImgOne from "./banner/bannerImgOne.webp";
+import bannerImgTwo from "./banner/bannerImgTwo.webp";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import saleImgOne from "./sale/saleImgOne.webp";
+import saleImgTwo from "./sale/saleImgTwo.webp";
+import saleImgThree from "./sale/saleImgThree.webp";
+import smartwatch1 from './banner/smartwatch1.jpg'
+// ============== Products Start here ====================
+import pdf1 from "../pdf/pdf1.pdf";
+// New Arrivals
+import newArrOne from "./products/newArrival/roundTableClock.jpg";
+import newArrTwo from "./products/newArrival/smartwatch.jpg";
+import newArrThree from "./products/newArrival/clothbasket.jpg";
+import newArrFour from "./products/newArrival/babytoys.jpg";
+import newArrFive from './products/newArrival/baby2.jpg';
+
+// Best Sellers
+import bestSellerOne from "./products/bestSeller/flowerbase.jpg";
+import bestSellerTwo from "./products/bestSeller/bag.jpeg";
+import bestSellerThree from "./products/bestSeller/household2.jpeg";
+import bestSellerFour from "./products/bestSeller/travelbag.jpg";
+import imprimante1 from "./products/bestSeller/imprimante1.webp";
+import imprimante2 from "./products/bestSeller/imprimante2.webp";
+import imprimante4 from "./products/bestSeller/imprimante4.webp";
+import imprimante5 from "./products/bestSeller/imprimante5.webp";
+import imprimante6 from "./products/bestSeller/imprimante6.webp";
+import imprimante7 from "./products/bestSeller/imprimante7.webp";
+import imprimante8 from "./products/bestSeller/imprimante8.webp";
+import imprimante9 from "./products/bestSeller/imprimante9.webp";
+import encre1 from "./products/bestSeller/encre1.webp";
+import encre2 from "./products/bestSeller/encre2.webp";
+import encre3 from "./products/bestSeller/encre3.webp";
+import encre4 from "./products/bestSeller/encre4.webp";
+import bac1 from "./products/bestSeller/bac1.webp";
+import bac2 from "./products/bestSeller/bac2.webp";
+import bac3 from "./products/bestSeller/bac3.webp";
+import bac4 from "./products/bestSeller/bac4.webp";
+import ruban1 from "./products/bestSeller/ruban1.webp";
+import ruban2 from "./products/bestSeller/ruban2.webp";
+import ruban3 from "./products/bestSeller/ruban3.webp";
+import ruban4 from "./products/bestSeller/ruban4.webp";
+
+// Special Offers
+import spfOne from "./products/specialOffer/inkcart.webp";
+import spfTwo from "./products/specialOffer/spfTwo.webp";
+import spfThree from "./products/specialOffer/spfThree.webp";
+import spfFour from "./products/specialOffer/spfFour.webp";
+import kidswatch from './products/specialOffer/kidswatch.avif'
+// Year Product
+import productOfTheYear from "./products/productOfTheYear.webp";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+import IMPRIMANTE_PANTUM_CP2200DW from "../images/products/imprimante/IMPRIMANTE_PANTUM_CP2200DW.webp";
+import IMPRIMANTE_PANTUM_BM5100FDW from "../images/products/imprimante/IMPRIMANTE_PANTUM_BM5100FDW.webp";
+import IMPRIMANTE_PANTUM_BP5100DN from "../images/products/imprimante/IMPRIMANTE_PANTUM_BP5100DN.webp";
+import IMPRIMANTE_PANTUM_M6609N from "../images/products/imprimante/IMPRIMANTE_PANTUM_M6609N.webp";
+import IMPRIMANTE_PANTUM_P3300DN from "../images/products/imprimante/IMPRIMANTE_PANTUM_P3300DN.webp";
+import IMPRIMANTE_PANTUM_P3300DW from "../images/products/imprimante/IMPRIMANTE_PANTUM_P3300DW.webp";
+import IMPRIMANTE_PANTUM_P2509W from "../images/products/imprimante/IMPRIMANTE_PANTUM_P2509W.webp";
+import IMPRIMANTE_PANTUM_M6559N from "../images/products/imprimante/IMPRIMANTE_PANTUM_M6559N.webp";
+import IMPRIMANTE_PANTUM_P2509 from "../images/products/imprimante/IMPRIMANTE_PANTUM_P2509.webp";
+import hp1 from "../images/products/imprimante/hp1.webp";
+import hp2 from "../images/products/imprimante/hp2.webp";
+import hp3 from "../images/products/imprimante/hp3.webp";
+
+import flowerVase from '../images/products/imprimante/flowerVase.jpg';
+import walClock from '../images/products/imprimante/walClock.webp';
+import tableLight from '../images/products/imprimante/tableLight.jpg'
+import wallMounts from '../images/products/imprimante/wallMounts.jpg'
+import showCase from '../images/products/imprimante/showCase.jpg'
+import stoneDecors from '../images/products/imprimante/stoneDecors.jpg'
+import mirror from '../images/products/imprimante/mirror.jpg'
+import kitchen1 from '../images/products/imprimante/kitchen1.webp'
+import kitchen2 from '../images/products/imprimante/kitchen2.jpg'
+import chimes from '../images/products/imprimante/chimes.jpg'
+import buddha from '../images/products/imprimante/buddha.jpg'
+import keyholder from '../images/products/imprimante/keyholder.webp'
+import frames from '../images/products/imprimante/frames.jpg'
+import disney from '../images/products/imprimante/disney.jpg'
+import krishna from '../images/products/imprimante/krishna.webp'
+import cuttingboard from '../images/products/imprimante/cuttingboard.jpg'
+import metalbox from '../images/products/imprimante/metalbox.webp'
+import kettle from '../images/products/imprimante/kettle.jpg'
+import fruitbasket from '../images/products/imprimante/fruitbasket.webp'
+import epoxy from '../images/products/imprimante/epoxy.jpg'
+import speaker from '../images/products/imprimante/speaker.jpg'
+import teapoy from '../images/products/imprimante/teapoy.jpg'
+import cups from '../images/products/imprimante/cups.webp'
+import humidifier from '../images/products/imprimante/humidifier.jpg'
+import tableclock from '../images/products/imprimante/tableclock.webp'
+import blackplate from '../images/products/imprimante/blackplate.webp'
+import incense from '../images/products/imprimante/incense.jpg'
+import oildispenser from '../images/products/imprimante/oildispenser.webp'
+import bottles from '../images/products/imprimante/bottles.webp'
+import carpets from '../images/products/imprimante/carpets.jpg'
+
+import ricoh1 from "../images/products/imprimante/ricoh1.webp";
+import ricoh2 from "../images/products/imprimante/ricoh2.webp";
+import ricoh3 from "../images/products/imprimante/ricoh3.webp";
+import ricoh4 from "../images/products/imprimante/ricoh4.webp";
+import ricoh5 from "../images/products/imprimante/ricoh5.webp";
+import espson1 from "../images/products/imprimante/espson1.webp";
+import espson2 from "../images/products/imprimante/espson2.webp";
+import neonlight from '../images/products/imprimante/neonlight.jpg'
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  smartwatch1,
+  // ===================== Products start here ============
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+  newArrFive,
+  flowerVase,
+  walClock,
+  tableLight,
+  wallMounts,
+  showCase,
+  stoneDecors,
+  mirror,
+  kitchen1,
+  kitchen2,
+  chimes,
+  buddha,
+  keyholder,
+  frames,
+  disney,
+  krishna,
+  cuttingboard,
+  metalbox,
+  kettle,
+  fruitbasket,
+  epoxy,
+  speaker,
+  teapoy,
+  cups,
+  humidifier,
+  neonlight,
+  tableclock,
+  blackplate,
+  incense,
+  oildispenser,
+  bottles,
+  carpets,
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+  imprimante1,
+  imprimante2,
+  imprimante4,
+  imprimante5,
+  imprimante6,
+  imprimante7,
+  imprimante8,
+  imprimante9,
+  encre1,
+  encre2,
+  encre3,
+  encre4,
+  bac1,
+  bac2,
+  bac3,
+  bac4,
+  ruban1,
+  ruban2,
+  ruban3,
+  ruban4,
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+  kidswatch,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+  pdf1,
+
+  // imprimante pantum
+  IMPRIMANTE_PANTUM_CP2200DW,
+  IMPRIMANTE_PANTUM_BM5100FDW,
+  IMPRIMANTE_PANTUM_BP5100DN,
+  IMPRIMANTE_PANTUM_M6609N,
+  IMPRIMANTE_PANTUM_P3300DN,
+  IMPRIMANTE_PANTUM_P3300DW,
+  IMPRIMANTE_PANTUM_P2509W,
+  IMPRIMANTE_PANTUM_M6559N,
+  IMPRIMANTE_PANTUM_P2509,
+
+  // imprimante Hp
+  hp1,
+  hp2,
+  hp3,
+
+  // imprimante Ricoh
+  ricoh1,
+  ricoh2,
+  ricoh3,
+  ricoh4,
+  ricoh5,
+  espson1,
+  espson2,
+};
